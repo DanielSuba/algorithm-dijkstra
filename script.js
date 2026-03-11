c24e36d0bed0986a39d25f7eefeb3079674610bb
DanielSuba <danielius2005m@gmail.com>
2026-03-11 16:28:34 +0200

    Dodane koncowe przyciski + suwak na przyszlosc

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// --- 1. Konfiguracja siatki ---
+// Poczatkowe zmienne
 const ROWS = document.getElementById('row');
 const COLS = document.getElementById('col');
 const gridContainer = document.getElementById('grid-container');
 
-// Dwuwymiarowa tablica do przechowywania logiki (dla algorytmu)
 let gridLogic = []; 
 
-// Pozycje startowa i końcowa 
+// Pozycje startowa i końcowe 
 const START_NODE = { row: 10, col: 5 };
 const END_NODE = { row: 10, col: 34 };
 
-// --- 2. Generowanie siatki ---
+// ------------------------ Generowanie siatki ----------------------
 function createGrid() {
-    // Ustawienie liczby kolumn w CSS Grid
     
 }
 
-// Uruchomienie generowania siatki po załadowaniu skryptu
+// Generowania siatki
 createGrid();
